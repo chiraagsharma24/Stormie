96b38aa0091e1becaa0c2504e6f28d43e9dcc0fa
Chirag Sharma <chiragnoida3345@gmail.com>
2025-04-09 03:14:31 +0530

    Added API KEY, Endpoints from OPenweather API

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,12 @@
 import { PropsWithChildren } from "react";
+import Header from "./header";
 
 export default function Layout({children}:PropsWithChildren){
     return(
         <div className="bg-gradient-to-br from-background to-muted">
-            header
+            <Header/>
             <main className="min-h-screen container mx-auto px-4 py-8">{children}</main>
-            <footer className="border-t backdrop-blur p">
+            <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
                 <div className="container mx-auto px-4 text-center text-gray-400">
                     <p>
                     Designed and Developed by Chirag Sharma
